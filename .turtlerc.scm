d7de4fda4ec3deaee865f86ce5e792b5544750cf
Abb1x <abb1x@protonmail.com>
2021-04-29 11:15:53 -0400

    Config: fixed typo

--- .turtlerc.scm
+++ .turtlerc.scm
@@ -3,6 +3,6 @@
 
 (define-public prompt (string-concatenate (list (colorize-string "%u " 'BLUE 'BOLD) (colorize-string "%d " 'CYAN 'BOLD) (colorize-string "λ " 'GREEN 'BOLD) (color 'RESET)))) ;; Green lambda
 
-(define-public prompt (string-concatenate (list (colorize-string "%u " 'BLUE 'BOLD) (colorize-string "%d " 'CYAN 'BOLD) (colorize-string "λ " 'RED 'BOLD) (color 'RESET)))) ;; red lambda
+(define-public failed-prompt (string-concatenate (list (colorize-string "%u " 'BLUE 'BOLD) (colorize-string "%d " 'CYAN 'BOLD) (colorize-string "λ " 'RED 'BOLD) (color 'RESET)))) ;; red lambda
 
 (display (string-concatenate (list "Welcome to " (color 'GREEN 'BOLD) "turtle" (color 'RESET) ", " (color 'CYAN 'BOLD) (getenv "USER") (color 'RESET) ".\nA little shell for " (color 'BLUE 'BOLD) "scheme " (color 'RESET) "enthusiasts.\n"))) ;; Welcome message
