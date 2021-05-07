142f9a6dce5bb8a932f792bd9f3e18d0d30911f5
d0p1 <contact@d0p1.eu>
2021-05-07 14:36:10 +0200

    Refactor: ISO C90 forbids mixed declarations and code

--- src/shell/lisp.c
+++ src/shell/lisp.c
@@ -1,18 +1,23 @@
 #include <stdlib.h>
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
 #include <libguile.h>
+#pragma GCC diagnostic pop
 
 char *lisp_get_var(char *name)
 {
-    SCM var = scm_c_public_variable("turtlerc", name);
+    SCM var;
+    SCM var_ref;
+    char* text;
+
+    var = scm_c_public_variable("turtlerc", name);
 
     if (scm_is_false(var))
     {
         return NULL;
     }
-
-    SCM var_ref = scm_variable_ref(var);
-
-    char* text = scm_to_utf8_string(var_ref);
+    var_ref = scm_variable_ref(var);
+    text = scm_to_utf8_string(var_ref);
     
     return text;
 }
